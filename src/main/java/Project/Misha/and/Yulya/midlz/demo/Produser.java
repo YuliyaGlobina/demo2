ea12bd2c1be245c602764c7cec0b8d7a9eb9fdc0
yuliy <y.globina@yandex.ru>
2023-07-01 17:44:18 +0300

    new

--- src/main/java/Project/Misha/and/Yulya/midlz/demo/Produser.java
+++ src/main/java/Project/Misha/and/Yulya/midlz/demo/Produser.java
@@ -1,9 +1,7 @@
 package Project.Misha.and.Yulya.midlz.demo;
 
-import org.apache.kafka.clients.consumer.*;
-import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.clients.producer.*;
 
-import java.util.Collections;
 import java.util.Properties;
 
 public class Produser {
@@ -12,29 +10,28 @@ public class Produser {
 
     public static void main(String[] args) {
 
-        // Настройки Kafka Consumer
+        // Настройки Kafka Producer
         Properties props = new Properties();
         props.put("bootstrap.servers", "localhost:9092");
-        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("group.id", "how_are_you_group");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
-        // Создание Kafka Consumer
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        // Создание Kafka Producer
+        Producer<String, String> producer = new KafkaProducer<>(props);
 
-        // Подписка на тему
-        consumer.subscribe(Collections.singletonList(TOPIC_NAME));
-
-        // Чтение сообщений из Kafka
-        while (true) {
-            ConsumerRecords<String, String> records = consumer.poll(100);
-            for (ConsumerRecord<String, String> record : records) {
-                System.out.println("Получено сообщение: " + record.value());
-                System.out.println("Отправка ответа: Как дела?");
+        // Отправка сообщения в Kafka
+        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, "Привет");
+        producer.send(record, new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata metadata, Exception exception) {
+                if (exception != null) {
+                    exception.printStackTrace();
+                } else {
+                    System.out.println("Сообщение успешно отправлено в тему: " + metadata.topic());
+                }
             }
+        });
 
-            // Подтверждение обработки сообщений
-            consumer.commitAsync();
-        }
+        producer.close();
     }
 }
